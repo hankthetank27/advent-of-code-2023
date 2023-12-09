63695c70b1b64301b8a53d13732e90ef47ea9796
Hank Jackson <hjackson277@gmail.com>
2023-12-08 21:36:51 -0500

    day1 refactor

--- day_1/src/lib.rs
+++ day_1/src/lib.rs
@@ -1,54 +1,55 @@
+use std::{str::Chars, u32};
+
 const SPELLED: [&str; 9] = [
     "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
 ];
 
+trait ProcessLine {
+    fn filter_nums(self) -> usize;
+    fn convert_words(self, line: &str) -> String;
+}
+
+impl ProcessLine for Chars<'_> {
+    fn filter_nums(self) -> usize {
+        let nums = self.filter(|char| char.is_digit(10)).collect::<Vec<char>>();
+
+        let first = nums.first().unwrap_or(&'0');
+        let last = nums.last().unwrap_or(&'0');
+
+        return format!("{first}{last}")
+            .parse::<usize>()
+            .expect("Should parse as `usize`.");
+    }
+
+    fn convert_words(self, line: &str) -> String {
+        return self
+            .enumerate()
+            .fold(String::new(), |mut digits, (i, char)| {
+                let substring = line.chars().skip(i).collect::<String>();
+                let digit_word = SPELLED
+                    .iter()
+                    .enumerate()
+                    .find(|(_, &w)| substring.starts_with(w));
+
+                match digit_word {
+                    Some((j, _)) => {
+                        digits.push(char::from_digit((j + 1) as u32, 10).unwrap_or('0'));
+                    }
+                    None => digits.push(char),
+                }
+                return digits;
+            });
+    }
+}
+
 pub fn part_1(input: &str) -> usize {
-    return input
-        .lines()
-        .map(|line| {
-            let nums = line
-                .chars()
-                .filter(|char| char.is_digit(10))
-                .collect::<Vec<char>>();
-
-            let first = nums.first().unwrap_or(&'0');
-            let last = nums.last().unwrap_or(&'0');
-
-            return format!("{first}{last}")
-                .parse::<usize>()
-                .expect("Should parse as `usize`.");
-        })
-        .sum();
+    return input.lines().map(|line| line.chars().filter_nums()).sum();
 }
 
 pub fn part_2(input: &str) -> usize {
     return input
         .lines()
-        .map(|line| {
-            let chars = line.as_bytes();
-            let nums = chars
-                .iter()
-                .enumerate()
-                .fold(Vec::new(), |mut acc, (i, char)| {
-                    if char.is_ascii_digit() {
-                        acc.push((char - b'0') as u32)
-                    } else {
-                        for (j, word) in SPELLED.iter().enumerate() {
-                            if chars[i..].starts_with(word.as_bytes()) {
-                                acc.push(j as u32 + 1);
-                            }
-                        }
-                    }
-                    return acc;
-                });
-
-            let first = nums.first().unwrap_or(&0);
-            let last = nums.last().unwrap_or(&0);
-
-            return format!("{first}{last}")
-                .parse::<usize>()
-                .expect("Should parse as `usize`.");
-        })
+        .map(|line| line.chars().convert_words(&line).chars().filter_nums())
         .sum();
 }
 
