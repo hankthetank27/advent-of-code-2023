ee2c3ccd7bae8095b09007d5c15c6e5a5979d9e1
Hank Jackson <hjackson277@gmail.com>
2023-12-10 12:49:23 -0500

    day 2 part 2 complete

--- day_2/src/lib.rs
+++ day_2/src/lib.rs
@@ -1,40 +1,77 @@
+use std::cmp;
+
 pub fn part_1(input: &str) -> usize {
-    return input
-        .lines()
-        .map(|game| {
-            let (id_str, reveals) = game.split_once(':').unwrap_or(("", ""));
-            let id = get_num(id_str);
-
-            let is_not_valid = reveals.split(";").find(|reveal| {
-                return reveal
-                    .split(',')
-                    .find(|cube| {
-                        let num = get_num(cube);
-                        return match cube.split_whitespace().nth(1).unwrap_or("invalid") {
-                            "red" => num > 12,
-                            "green" => num > 13,
-                            "blue" => num > 14,
-                            _ => true,
-                        };
-                    })
-                    .is_some();
-            });
-
-            return match is_not_valid {
-                Some(_) => 0,
-                None => id,
-            };
-        })
-        .sum();
+    return parse_games(input)
+        .iter()
+        .enumerate()
+        .fold(0, |sum, (i, game)| {
+            let is_not_valid = game.iter().find(|set| !set.is_valid()).is_some();
+            sum + match is_not_valid {
+                true => 0,
+                false => i + 1,
+            }
+        });
 }
 
-fn get_num(word: &str) -> usize {
-    return word
-        .chars()
-        .filter(|char| char.is_digit(10))
-        .collect::<String>()
-        .parse::<usize>()
-        .unwrap_or(0);
+pub fn part_2(input: &str) -> usize {
+    return parse_games(input).iter().fold(0, |sum, game| {
+        let max_cube = game.iter().fold(CubeSet::new(), |mut max_cube, cube| {
+            max_cube.red = cmp::max(cube.red, max_cube.red);
+            max_cube.blue = cmp::max(cube.blue, max_cube.blue);
+            max_cube.green = cmp::max(cube.green, max_cube.green);
+            return max_cube;
+        });
+        return sum + (max_cube.red * max_cube.blue * max_cube.green);
+    });
+}
+
+#[derive(Debug)]
+struct CubeSet {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+impl CubeSet {
+    fn new() -> Self {
+        CubeSet {
+            red: 0,
+            green: 0,
+            blue: 0,
+        }
+    }
+
+    fn is_valid(&self) -> bool {
+        self.red <= 12 && self.green <= 13 && self.blue <= 14
+    }
+}
+
+fn parse_games(games: &str) -> Vec<Vec<CubeSet>> {
+    return games
+        .lines()
+        .map(|game| {
+            let sets = game.split_once(':').unwrap().1;
+            return sets
+                .split(";")
+                .map(|set| {
+                    return set.split(',').fold(CubeSet::new(), |mut cube_set, cube| {
+                        let mut cube_iter = cube.split_whitespace();
+                        let num = cube_iter.next().unwrap().parse::<usize>().unwrap();
+                        let color = cube_iter.next().unwrap();
+
+                        match color {
+                            "red" => cube_set.red = num,
+                            "green" => cube_set.green = num,
+                            "blue" => cube_set.blue = num,
+                            _ => unreachable!(),
+                        };
+
+                        return cube_set;
+                    });
+                })
+                .collect::<Vec<CubeSet>>();
+        })
+        .collect::<Vec<Vec<CubeSet>>>();
 }
 
 #[cfg(test)]
@@ -45,5 +82,11 @@ mod test {
     fn test01() {
         let input = include_str!("../input_example_1.txt");
         assert_eq!(part_1(input), 8);
+    }
+
+    #[test]
+    fn test02() {
+        let input = include_str!("../input_example_1.txt");
+        assert_eq!(part_2(input), 2286);
     }
 }
