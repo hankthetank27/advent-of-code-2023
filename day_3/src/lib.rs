7b12a62467cde1362b299290f6f29d2b23956930
Hank Jackson <hjackson277@gmail.com>
2023-12-15 12:15:04 -0500

    completed day 3

--- day_3/src/lib.rs
+++ day_3/src/lib.rs
@@ -1,53 +1,105 @@
-use std::collections::HashSet;
+use std::{collections::HashMap, usize};
 
 pub fn part_1(input: &str) -> usize {
-    let mut symbol_map = HashSet::new();
-
-    for (i, line) in input.lines().enumerate() {
-        for (j, char) in line.char_indices() {
-            if !char.is_ascii_digit() && char != '.' {
-                symbol_map.insert((i as isize, j as isize));
-            }
-        }
-    }
-
-    return input.lines().enumerate().fold(0, |mut sum, (i, line)| {
-        let mut curr_num = String::new();
-        let mut curr_idx = -1;
-
-        for (j, char) in line.char_indices() {
-            if char.is_ascii_digit() {
-                curr_num.push(char);
-            } else {
-                if !curr_num.is_empty() {
-                    let curr = (curr_idx, curr_num.len() as isize + curr_idx);
-                    if check_ajacent(i, curr, &symbol_map) {
-                        sum += curr_num.parse::<usize>().unwrap();
-                    }
-                }
-                curr_num.clear();
-                curr_idx = j as isize;
-            }
-        }
-
-        let curr = (curr_idx, curr_num.len() as isize + curr_idx);
-        if !curr_num.is_empty() && check_ajacent(i, curr, &symbol_map) {
-            sum += curr_num.parse::<usize>().unwrap();
-        }
-
-        sum
+    let symbol_map = map_symbols(input);
+    return input.lines().enumerate().fold(0, |sum, (i, line)| {
+        sum + find_nums(line)
+            .iter()
+            .filter(|num| !num.find_adjacent_symbols(i, &symbol_map).is_empty())
+            .map(|part_num| part_num.val)
+            .sum::<usize>()
     });
 }
 
-fn check_ajacent(line_idx: usize, num_idx: (isize, isize), map: &HashSet<(isize, isize)>) -> bool {
-    for i in (line_idx as isize - 1)..=(line_idx as isize + 1) {
-        for j in num_idx.0..=num_idx.1 + 1 {
-            if map.contains(&(i as isize, j as isize)) {
-                return true;
-            };
+pub fn part_2(input: &str) -> usize {
+    let symbol_map = map_symbols(input);
+    return input
+        .lines()
+        .enumerate()
+        .fold(HashMap::new(), |mut map, (i, line)| {
+            for num in find_nums(line).iter() {
+                let adjacent = num.find_adjacent_symbols(i, &symbol_map);
+                for &pos in adjacent.iter() {
+                    if symbol_map.get(&pos) == Some(&'*') {
+                        map.entry(pos).or_insert(Vec::new()).push(num.val);
+                    };
+                }
+            }
+            return map;
+        })
+        .iter()
+        .filter(|(_, nums)| nums.len() == 2)
+        .map(|(_, nums)| nums.iter().product::<usize>())
+        .sum();
+}
+
+struct NumMatch {
+    val: usize,
+    start: usize,
+    end: usize,
+}
+
+impl NumMatch {
+    fn find_adjacent_symbols(
+        &self,
+        line: usize,
+        symbols: &HashMap<(usize, usize), char>,
+    ) -> Vec<(usize, usize)> {
+        let mut adjacent = vec![];
+        for i in line.saturating_sub(1)..=line + 1 {
+            for j in self.start.saturating_sub(1)..=self.end + 1 {
+                match symbols.get(&(i, j)) {
+                    Some(_) => adjacent.push((i, j)),
+                    None => (),
+                };
+            }
+        }
+        return adjacent;
+    }
+}
+
+fn map_symbols(input: &str) -> HashMap<(usize, usize), char> {
+    let mut symbol_map = HashMap::new();
+    for (i, line) in input.lines().enumerate() {
+        for (j, char) in line.char_indices() {
+            if !char.is_digit(10) && char != '.' {
+                symbol_map.insert((i, j), char);
+            }
         }
     }
-    return false;
+    return symbol_map;
+}
+
+fn find_nums(input: &str) -> Vec<NumMatch> {
+    let mut res = vec![];
+    let mut num = String::new();
+    let mut left = 0;
+
+    for (right, char) in input.char_indices() {
+        if char.is_digit(10) {
+            num.push(char);
+        } else {
+            if !num.is_empty() {
+                res.push(NumMatch {
+                    val: num.parse::<usize>().unwrap(),
+                    start: left,
+                    end: num.len() + left - 1,
+                });
+                num.clear();
+            }
+            left = right + 1;
+        }
+    }
+
+    if !num.is_empty() {
+        res.push(NumMatch {
+            val: num.parse::<usize>().unwrap(),
+            start: left,
+            end: num.len() + left - 1,
+        });
+    }
+
+    return res;
 }
 
 #[cfg(test)]
@@ -58,5 +110,11 @@ mod test {
     fn test01() {
         let input = include_str!("../input_example_1.txt");
         assert_eq!(part_1(input), 4361);
+    }
+
+    #[test]
+    fn test02() {
+        let input = include_str!("../input_example_1.txt");
+        assert_eq!(part_2(input), 467835);
     }
 }
--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,9 +1,9 @@
-use day_3::part_1;
+use day_3::{part_1, part_2};
 
 fn main() {
     let input = include_str!("../input.txt");
     let answer_one = part_1(input);
-    // let answer_two = part_2(input);
+    let answer_two = part_2(input);
     println!("answer part 1: {answer_one}");
-    // println!("answer part 2: {answer_two}");
+    println!("answer part 2: {answer_two}");
 }
